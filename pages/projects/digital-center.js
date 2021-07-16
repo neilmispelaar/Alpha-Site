f5d3644731e4e9eb8c48eba2a9d48b4199d515f1
Paul Craig <paul@pcraig3.ca>
2021-07-16 11:16:51 -0400

    Update content and keys for digital center page

--- pages/projects/digital-center.js
+++ pages/projects/digital-center.js
@@ -9,14 +9,15 @@ import { CallToAction } from "../../components/molecules/CallToAction";
 function ThumbnailWithCaption({
   title = "Image 1",
   src = "https://uhdwallpapers.org/uploads/converted/20/01/14/the-mandalorian-5k-1920x1080_477555-mm-90.jpg",
-  caption = "The Service Canada homepage serves over (xxx,xxx) people a year.",
+  alt = "",
+  children,
 }) {
   return (
-    <div>
+    <div className="flex justify-center flex-col">
       <h3 className="text-lg mt-3">{title}</h3>
       <figure className="shadow-experiment-shadow">
-        <img src={src} alt="" className="border-b-2" />
-        <figcaption className="text-base p-8">{caption}</figcaption>
+        <img src={src} alt={alt} className="border-b-2" />
+        <figcaption className="text-base p-8">{children}</figcaption>
       </figure>
     </div>
   );
@@ -27,9 +28,6 @@ ThumbnailWithCaption.propTypes = {
   src: PropTypes.string,
   caption: PropTypes.string,
 };
-
-const imgSrc =
-  "../dc-1.1.jpg";
 
 export default function Home(props) {
   const { t } = useTranslation(["common", "dc"]);
@@ -59,48 +57,51 @@ export default function Home(props) {
       <section className="layout-container mb-10">
         <h1
           id="pageMainTitle"
-          className="mb-10 text-h1l font-bold sm:w-max"
+          className="mb-10 mt-10 text-h1l font-bold sm:w-max"
           tabIndex="-1"
         >
-          {t("dc:h1")}
+          {t("dc:OverviewTitle")}
         </h1>
-        <p className="text-base mt-3">{t("dc:intro-1")}</p>
-        <p className="text-base mt-3">{t("dc:intro-2")}</p>
+        <p className="text-base mt-3">{t("dc:ProductGoal1")}</p>
+        <p className="text-base mt-3">{t("dc:ProductGoal2")}</p>
 
-        <h2 className="text-lg mt-8">{t("dc:h2-1")}</h2>
-        <p className="text-base mt-6">{t("dc:concept1-1")}</p>
-        <p className="text-base mt-6">{t("dc:concept1-2")}</p>
-        <p className="text-base mt-6">{t("dc:concept1-3")}</p>
-        
+        <h2 className="text-lg mt-8">{t("dc:Concept1Heading")}</h2>
+        <p className="text-base mt-6">{t("dc:Concept1P1")}</p>
+        <p className="text-base mt-6">{t("dc:Concept1P2")}</p>
+        <p className="text-base mt-6">{t("dc:Concept1P3")}</p>
+
         <div className="mx-auto">
-
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-
-          <div class="flex justify-center">
-          <ThumbnailWithCaption
-            title={t("dc:concept1-imgTitle-1")}
-            src={imgSrc}
-            caption={t("dc:concept1-imgCaption-1")}
-          />
+          <div className="mt-6 grid grid-cols-1 lg:grid-cols-2 items-start gap-6">
+            <ThumbnailWithCaption
+              title={t("dc:Concept1ImgTitle1")}
+              alt={t("dc:Concept1ImgAlt1")}
+              src={"/dc-1.1.jpg"}
+            >
+              <div>{t("dc:Concept1ImgCaption1")}</div>
+              <ol className="ml-2 mt-2">
+                <li className="mt-1">{t("dc:Concept1ImgCaption1LI1")}</li>
+                <li className="mt-1">{t("dc:Concept1ImgCaption1LI2")}</li>
+                <li className="mt-1">{t("dc:Concept1ImgCaption1LI3")}</li>
+                <li className="mt-1">{t("dc:Concept1ImgCaption1LI4")}</li>
+                <li className="mt-1">{t("dc:Concept1ImgCaption1LI5")}</li>
+                <li className="mt-1">{t("dc:Concept1ImgCaption1LI6")}</li>
+              </ol>
+            </ThumbnailWithCaption>
+            <ThumbnailWithCaption
+              title={t("dc:Concept1ImgTitle2")}
+              alt={t("dc:Concept1ImgAlt2")}
+              src={"/dc-1.2.jpg"}
+            >
+              <div>{t("dc:Concept1ImgCaption2")}</div>
+            </ThumbnailWithCaption>
+            <ThumbnailWithCaption
+              title={t("dc:Concept1ImgTitle3")}
+              alt={t("dc:Concept1ImgAlt3")}
+              src={"/dc-1.3.jpg"}
+            >
+              <div>{t("dc:Concept1ImgCaption3")}</div>
+            </ThumbnailWithCaption>
           </div>
-
-           <div class="flex justify-center">
-          <ThumbnailWithCaption
-            title={"Image 2: Homepage"}
-            src={imgSrc}
-            caption={t("dc:concept1-imgCaption-1")}
-          />
-          </div>
-
-           <div class="flex justify-center">
-          <ThumbnailWithCaption
-            title={"Image 3: Homepage"}
-            src={imgSrc}
-            caption={t("dc:concept1-imgCaption-1")}
-          />
-          </div>
-
-        </div>
         </div>
       </section>
 
